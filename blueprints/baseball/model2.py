783b056c1474a2e01bd3e2c93f2839a0dcd65197
kyle-black <kpblack87@gmail.com>
2023-05-25 11:51:04 -0500

    model_time

--- blueprints/baseball/model2.py
+++ blueprints/baseball/model2.py
@@ -8,6 +8,7 @@ from data_fetcher import fetch_and_store_data
 from apscheduler.schedulers.blocking import BlockingScheduler
 import os
 from urllib.parse import urlparse
+from datetime import datetime
 
 
 #REDIS_HOST = "127.0.0.1"  # Replace with your Redis server's IP address or hostname
@@ -195,9 +196,12 @@ def main():
     
 
 if __name__ in "__main__":
+    current_time = datetime.now().time()
+    print("StartTime:",current_time)
     scheduler = BlockingScheduler()
     scheduler.add_job(main, 'interval', minutes=5)
     scheduler.start()
+    print("Pull Time",current_time)
     
     #try:
     #    main()
