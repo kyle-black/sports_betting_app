1d0482a46f56136e13fc3562eb6f1f5fb4d87da4
kyle-black <kpblack87@gmail.com>
2023-05-30 15:53:26 -0500

    fix 500 error

--- app.py
+++ app.py
@@ -82,10 +82,10 @@ def create_app(config_name):
     # note that we set the 404 status explicitly
         return render_template('404.html'), 404
 
-    @app.errorhandler(500)
-    def internal_server_error(e):
+   # @app.errorhandler(500)
+   # def internal_server_error(e):
     # note that we set the 500 status explicitly
-        return render_template('500.html'), 500
+   #     return render_template('500.html'), 500
     
     return app
 
