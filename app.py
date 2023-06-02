de376d8d63a6bc7712b6fc16b921b2e9fae5c706
kyle-black <kpblack87@gmail.com>
2023-06-02 14:24:59 -0500

    import logging

--- app.py
+++ app.py
@@ -24,12 +24,27 @@ import requests
 import json
 import jsonify
 from config import config_dict
-
+import logging
+from logging.handlers import RotatingFileHandler
 
 def create_app(config_name):
 
     app = Flask(__name__)
     app.config.from_object(config_dict[config_name])
+
+    if not app.debug:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/your_application.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Your application startup')
+
+
+
     
     redis_client = FlaskRedis(app)
 
