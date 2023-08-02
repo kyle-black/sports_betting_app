b13a9994f7782f4b858efa5ab7c6d949cc19d4f2
kyle-black <kpblack87@gmail.com>
2023-08-02 13:59:44 -0500

    tester

--- Run_Model/test_redis.py
+++ Run_Model/test_redis.py
@@ -5,16 +5,21 @@ import json
 
 url = os.getenv('REDIS_URL')
 
+def tester_():
+    redis_url = os.getenv('REDIS_URL')
+    parsed_url = urlparse(redis_url)
+    redis_client = redis.Redis(host=parsed_url.hostname, port=parsed_url.port, password=parsed_url.password)
 
-redis_url = os.getenv('REDIS_URL')
-parsed_url = urlparse(redis_url)
-redis_client = redis.Redis(host=parsed_url.hostname, port=parsed_url.port, password=parsed_url.password)
+    redis_key = 'run_stats'
 
-redis_key = 'mlb_data'
+    redis_data_raw = redis_client.get(redis_key)
 
-redis_data_raw = redis_client.get(redis_key)
+    redis_data = json.loads(redis_data_raw.decode('utf-8'))
 
-redis_data = json.loads(redis_data_raw.decode('utf-8'))
+    return redis_data
+    #print(redis_data)
 
 
-print(redis_data)
+
+if __name__ in "__main__":
+    tester_()
